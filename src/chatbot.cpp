99d784cb43f3978af4d9955d405ed0ca63e123c1
yugoo90 <uosuji54@gmail.com>
2022-12-07 13:09:09 -0700

    finished task 5, program runs without seg faulting, but still needs some clean up

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -38,7 +38,7 @@ ChatBot::~ChatBot()
     // deallocate heap memory
     if(_image != NULL) // Attention: wxWidgets used NULL and not nullptr
     {
-        delete _image;
+       //delete _image;
         _image = NULL;
     }
 }
